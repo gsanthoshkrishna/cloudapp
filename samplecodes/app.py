c0273caea7e109d2dff6d477efa27fb695489ae2
gowripriyavilla <gowripriyavilla@gmail.com>
2024-05-23 17:40:17 +0530

    samplecodecreated

---
+++ samplecodes/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+# Configure your MySQL connection
+db_config = {
+    'user': 'root',
+    'password': 'gowri@311',
+    'host': 'localhost',
+    'database': 'cloudapp'
+}
+
+@app.route('/')
+def index():
+    # Connect to the database
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    # Execute the query to get name and grade
+    query = "SELECT res_id,res_name,res_image FROM resource"
+    cursor.execute(query)
+
+    # Fetch all the results
+    res_list = cursor.fetchall()
+
+    # Close the connection
+    cursor.close()
+    conn.close()
+
+    # Pass the data to the template
+    return render_template('properties2.html', resources=res_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
