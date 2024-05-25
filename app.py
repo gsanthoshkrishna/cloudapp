8b607bd8ba82c033fa45cc2b9b2f7ef1c10e20bf
uma.repo <umabhavani8.gmail.com>
2024-05-25 15:20:39 +0530

    updated

--- app.py
+++ app.py
@@ -3,25 +3,34 @@ import mysql.connector
 
 app = Flask(__name__)
 
-# MySQL Configuration
-mysql_config = {
-    'host': 'localhost',
+# Configure your MySQL connection
+db_config = {
     'user': 'root',
     'password': 'Pass@123',
+    'host': 'localhost',
     'database': 'cloudapp'
 }
 
-# Connect to MySQL
-conn = mysql.connector.connect(**mysql_config)
-cursor = conn.cursor(dictionary=True)
-
-# Route for the main page
 @app.route('/')
 def index():
-    # Fetch resource-details names from the database
-    cursor.execute("SELECT res_name  FROM resources")
-    resources = [resources['res_name'] for resource in cursor.fetchall()]
-    return render_template('resrc.html', resources=resources)
+    # Connect to the database
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    # Execute the query to get name and grade
+    query = "SELECT res_id,res_name,res_image FROM resources"
+    cursor.execute(query)
+
+    # Fetch all the results
+    res_list = cursor.fetchall()
+
+    # Close the connection
+    cursor.close()
+    conn.close()
+
+
+    # Pass the data to the template
+    return render_template('properties2.html', resources=res_list)
 
 # Route for displaying student details
 @app.route('/resource_details', methods=['POST'])
@@ -29,12 +38,20 @@ def resource_details():
     # Get the selected student name from the form
     resource_name = request.form['resource']
 
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    
     # Query the database for the student details
-    cursor.execute("SELECT * FROM resources WHERE name = %s", (res_name,))
-    resource = cursor.fetchone()
+    cursor.execute("SELECT * FROM resourse_prop WHERE res_id = %s", (resource_name,))
+    resource = cursor.fetchall()
     print(resource)
 
-    return render_template('resrc.html', resource_details=resource)
+    # Close the connection
+    cursor.close()
+    conn.close()
+
+    return render_template('properties2.html', res_props=resource)
 
 if __name__ == '__main__':
     app.run(debug=True)
