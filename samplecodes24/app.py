50c3cb0399ba1d8b4c8dcf910056b2e287831c7c
gowripriyavilla <gowripriyavilla@gmail.com>
2024-05-29 15:21:47 +0530

    task24submitted

--- samplecodes24/app.py
+++ samplecodes24/app.py
@@ -12,20 +12,20 @@ dbconfig = {
     "database": "cloudapp"
 }
 
-cnxpool = mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool",
-                                                      pool_size=5,
-                                                      **dbconfig)
+cnxpool = mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool", pool_size=10, **dbconfig)
 
 @app.route('/')
 def index():
+    cnx = None
     try:
-        # Fetch resources from the resource table
         cnx = cnxpool.get_connection()
         cursor = cnx.cursor(dictionary=True)
+        
+        # Fetch resources from the resource table
         query = "SELECT res_id AS id, res_name AS name, res_image AS image FROM resource"
         cursor.execute(query)
         resources = cursor.fetchall()
-
+        
         # Fetch images from the tr_template_load table
         query = """
             SELECT DISTINCT tr_template_load.res_unique_id, tr_template_load.res_id, resource.res_image
@@ -34,33 +34,38 @@ def index():
         """
         cursor.execute(query)
         load_images = cursor.fetchall()
-
-        cursor.close()
-        cnx.close()
-
+        
         return render_template('task24.html', resources=resources, load_images=load_images)
     except mysql.connector.Error as err:
         return str(err)
+    finally:
+        if cursor:
+            cursor.close()
+        if cnx:
+            cnx.close()
 
 @app.route('/load_properties', methods=['GET'])
 def load_properties():
-    resource_id = request.args.get('resource_id')
-    print("Received resource_id for load:", resource_id)
+    res_unique_id = request.args.get('res_unique_id')
+    cnx = None
     try:
         cnx = cnxpool.get_connection()
         cursor = cnx.cursor(dictionary=True)
         query = """
             SELECT prop_name, prop_value 
             FROM tr_template_load 
-            WHERE res_id = %s
+            WHERE res_unique_id = %s
         """
-        cursor.execute(query, (resource_id,))
+        cursor.execute(query, (res_unique_id,))
         props = cursor.fetchall()
-        cursor.close()
-        cnx.close()
         return jsonify(props)
     except mysql.connector.Error as err:
         return str(err)
+    finally:
+        if cursor:
+            cursor.close()
+        if cnx:
+            cnx.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
