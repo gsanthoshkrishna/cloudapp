2058f7e1e6708440f361e7a680ba7d83e0ae55f4
gowripriyavilla <gowripriyavilla@gmail.com>
2024-06-01 15:32:43 +0530

    config.jsonfile

--- samplecodes24/app.py
+++ samplecodes24/app.py
@@ -1,32 +1,38 @@
 from flask import Flask, request, jsonify, render_template
+import json
 import mysql.connector
 from mysql.connector import pooling
 
 app = Flask(__name__)
 
-# Configure the MySQL connection pool
-dbconfig = {
-    "host": "localhost",
-    "user": "root",
-    "password": "gowri@311",
-    "database": "cloudapp"
-}
+# Load MySQL configuration from config.json
+with open('config.json') as config_file:
+    config = json.load(config_file)['mysql']
 
-cnxpool = mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool", pool_size=10, **dbconfig)
+# Create connection pool
+cnxpool = pooling.MySQLConnectionPool(
+    pool_name="mypool",
+    pool_size=5,
+    **config
+)
 
+# Define index route
 @app.route('/')
 def index():
     cnx = None
+    cursor = None
     try:
+        # Get connection from connection pool
         cnx = cnxpool.get_connection()
         cursor = cnx.cursor(dictionary=True)
+        # Perform database operations here
         
-        # Fetch resources from the resource table
+        # Example: Fetch resources from the resource table
         query = "SELECT res_id AS id, res_name AS name, res_image AS image FROM resource"
         cursor.execute(query)
         resources = cursor.fetchall()
         
-        # Fetch images from the tr_template_load table
+        # Example: Fetch images from the tr_template_load table
         query = """
             SELECT DISTINCT tr_template_load.res_unique_id, tr_template_load.res_id, resource.res_image
             FROM tr_template_load
@@ -44,15 +50,20 @@ def index():
         if cnx:
             cnx.close()
 
+# Define get_properties route
 @app.route('/get_properties', methods=['GET'])
 def get_properties():
-
-    res_unique_id = request.args.get('selected_id')
-    div_id = request.args.get('div_id')
     cnx = None
+    cursor = None
     try:
+        # Get connection from connection pool
         cnx = cnxpool.get_connection()
         cursor = cnx.cursor(dictionary=True)
+        # Perform database operations here
+        
+        res_unique_id = request.args.get('selected_id')
+        div_id = request.args.get('div_id')
+        
         query = """
           SELECT prop_name, prop_input_type, is_mandatory 
           FROM resource_prop 
@@ -64,9 +75,7 @@ def get_properties():
             FROM tr_template_load tpl
             JOIN resource_prop rp ON tpl.prop_id = rp.res_prop_id
             WHERE tpl.res_unique_id = %s;
-
             """
-        print(res_unique_id)
         cursor.execute(query, (res_unique_id,))
         props = cursor.fetchall()
         return jsonify(props)
@@ -77,7 +86,6 @@ def get_properties():
             cursor.close()
         if cnx:
             cnx.close()
-
 
 if __name__ == "__main__":
     app.run(debug=True)
